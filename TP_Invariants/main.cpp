d8c765aef3bb60c008ca0b8c8a427c6e111567a6
Dyl-DJM <dyl.dejesusmilitar@gmail.com>
2023-12-19 11:32:47 +0100

    Made the program work on many scenes

--- TP_Invariants/main.cpp
+++ TP_Invariants/main.cpp
@@ -15,33 +15,41 @@ int main(int argc, char **argv)
     typedef Object<DT26_6, Z3i::DigitalSet> DigitalObj3D;
     typedef ImageSelector<Z3i::Domain, int>::Type Image3D;
 
-    // read a 3D image
-    Image3D image = VolReader<Image3D>::importVol("../3D_images/dragon-128.vol");
+    // 3D scene storage
+    std::vector<std::string> scenes3D = {"fertility", "dragon"};
 
-    // make the foreground and background digital sets from the image
-    Z3i::DigitalSet set_foreground(image.domain()); // Create a digital set of proper size
-    // Z3i::DigitalSet set_background ( image.domain() );
-    SetFromImage<Z3i::DigitalSet>::append<Image3D>(set_foreground, image, 0, 255); // populate a digital set from the input image
-    // SetFromImage<Z3i::DigitalSet>::append<Image3D>(set_background, image, -1, 1);
+    // I want my programm to iterate on all the scenes 
+    for(const auto &scene : scenes3D){
+        std::cout << "\033[1;32mbold ===" + scene + " analysis ===\033[0m" << std::endl;
 
-    // =================== STEP 2 ===================
-    DigitalObj3D digitalObj(dt26_6, set_foreground);
-    std::vector<DigitalObj3D> objects;                                      // All connected components are going to be stored in it
-    std::back_insert_iterator<std::vector<DigitalObj3D>> inserter(objects); // Iterator used to populated "objects".
+        // read a 3D image
+        Image3D image = VolReader<Image3D>::importVol("../3D_images/" + scene + "-128.vol");
 
-    unsigned int nbc = digitalObj.writeComponents(inserter);
+        // make the foreground and background digital sets from the image
+        Z3i::DigitalSet set_foreground(image.domain()); // Create a digital set of proper size
+        // Z3i::DigitalSet set_background ( image.domain() );
+        SetFromImage<Z3i::DigitalSet>::append<Image3D>(set_foreground, image, 0, 255); // populate a digital set from the input image
+        // SetFromImage<Z3i::DigitalSet>::append<Image3D>(set_background, image, -1, 1);
 
-    std::cout << "Number of components : " << nbc << std::endl;
+        // =================== STEP 2 ===================
+        DigitalObj3D digitalObj(dt26_6, set_foreground);
+        std::vector<DigitalObj3D> objects;                                      // All connected components are going to be stored in it
+        std::back_insert_iterator<std::vector<DigitalObj3D>> inserter(objects); // Iterator used to populated "objects".
 
-    // 3D viewer
-    QApplication application(argc, argv);
+        unsigned int nbc = digitalObj.writeComponents(inserter);
 
-    typedef Viewer3D<> MyViewer;
-    MyViewer viewer;
-    viewer.show();
-    // viewer << shape;
-    viewer << SetMode3D(image.domain().className(), "BoundingBox");
-    viewer << set_foreground << image.domain() << MyViewer::updateDisplay;
+        std::cout << "Number of components : " << nbc << std::endl;
 
-    return application.exec();
+        // 3D viewer
+        QApplication application(argc, argv);
+
+        typedef Viewer3D<> MyViewer;
+        MyViewer viewer;
+        viewer.show();
+        // viewer << shape;
+        viewer << SetMode3D(image.domain().className(), "BoundingBox");
+        viewer << set_foreground << image.domain() << MyViewer::updateDisplay;
+
+        application.exec();
+    }
 }
