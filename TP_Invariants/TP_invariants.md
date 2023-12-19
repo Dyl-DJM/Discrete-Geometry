d1dc581addcd967753b20ffb31534d701c26cb54
Dyl-DJM <dyl.dejesusmilitar@gmail.com>
2023-12-19 18:23:44 +0100

    Ended the Step 2

--- TP_Invariants/TP_invariants.md
+++ TP_Invariants/TP_invariants.md
@@ -13,3 +13,49 @@
 
 ### STEP 2: CALCULATE THE NUMBERS OF CONNECTED COMPONENTS AND CAVITIES
 
+By taking the following pairs adjencies for the foreground andf the background : 
+
+- First (26 Fg, 6 Bg)
+- Second (6 Fg, 26 Bg)
+
+
+I used the WriteComponents method to check the connected components, and I ran my programm on all my images to get the following results : 
+
+
+```cpp
+ === fertility analysis ===
+(26, 6) Adjency : 
+Number of components : 1
+Number of cavities : 1
+(6, 26) Adjency : 
+Number of components : 1
+Number of cavities : 1
+
+ === dragon analysis ===
+(26, 6) Adjency : 
+Number of components : 1
+Number of cavities : 1
+(6, 26) Adjency : 
+Number of components : 2
+Number of cavities : 1
+
+ === filigree analysis ===
+(26, 6) Adjency : 
+Number of components : 1
+Number of cavities : 1
+(6, 26) Adjency : 
+Number of components : 1
+Number of cavities : 1
+
+ === torus analysis ===
+(26, 6) Adjency : 
+Number of components : 1
+Number of cavities : 1
+(6, 26) Adjency : 
+Number of components : 1
+Number of cavities : 1
+```
+
+
+### STEP 3: CALCULATE THE EULER CHARACTERISTIC
+
--- TP_Invariants/main.cpp
+++ TP_Invariants/main.cpp
@@ -6,39 +6,56 @@
 #include <DGtal/io/viewers/Viewer3D.h>
 #include <DGtal/images/imagesSetsUtils/SetFromImage.h>
 
+/* Namespaces */
 using namespace std;
 using namespace DGtal;
 using namespace Z3i;
 
+/* Types */
+typedef Object<DT26_6, Z3i::DigitalSet> DigObj26_6; // STEP 2 : 3D digital object
+typedef Object<DT6_26, Z3i::DigitalSet> DigObj6_26; // STEP 2 : 3D digital object
+
+/* STEP 2 : Get the number of components, thanks to WriteComponents() */
+template <typename Object>
+unsigned int getNbComponents(const Object &digitalObj, std::vector<Object> &objects)
+{
+    std::back_insert_iterator<std::vector<Object>> inserter(objects); // Iterator used to populated "objects".
+    unsigned int nbc = digitalObj.writeComponents(inserter);
+    return nbc;
+}
+
+template <typename ObjectType, typename Adjency>
+void ComponentsCavities(const std::string &&title,
+                        const Z3i::DigitalSet &set_foreground,
+                        const Z3i::DigitalSet &set_background,
+                        const Adjency &set)
+{
+    // Foreground
+    ObjectType DOForeground(set, set_foreground);
+    std::vector<ObjectType> objectsForeground; // All connected components are going to be stored in it
+    auto nbcForeground = getNbComponents(DOForeground, objectsForeground);
+
+    // Background
+    ObjectType DOBackground(set, set_background);
+    std::vector<ObjectType> objectsBackground; // All connected components are going to be stored in it
+    auto nbcBackground = getNbComponents(DOBackground, objectsBackground);
+
+    std::cout << title << std::endl;
+    std::cout << "Number of components : " << nbcForeground << std::endl;
+    std::cout << "Number of cavities : " << nbcBackground << std::endl;
+}
+
 int main(int argc, char **argv)
 {
-    typedef Object<DT26_6, Z3i::DigitalSet> DigitalObj3D;
     typedef ImageSelector<Z3i::Domain, int>::Type Image3D;
 
     // 3D scene storage
-    std::vector<std::string> scenes3D = {"fertility", "dragon"};
+    std::vector<std::string> scenes3D = {"fertility", "dragon", "filigree", "torus"};
 
-    // I want my programm to iterate on all the scenes 
-    for(const auto &scene : scenes3D){
-        std::cout << "\033[1;32mbold ===" + scene + " analysis ===\033[0m" << std::endl;
-
-        // read a 3D image
-        Image3D image = VolReader<Image3D>::importVol("../3D_images/" + scene + "-128.vol");
-
-        // make the foreground and background digital sets from the image
-        Z3i::DigitalSet set_foreground(image.domain()); // Create a digital set of proper size
-        // Z3i::DigitalSet set_background ( image.domain() );
-        SetFromImage<Z3i::DigitalSet>::append<Image3D>(set_foreground, image, 0, 255); // populate a digital set from the input image
-        // SetFromImage<Z3i::DigitalSet>::append<Image3D>(set_background, image, -1, 1);
-
-        // =================== STEP 2 ===================
-        DigitalObj3D digitalObj(dt26_6, set_foreground);
-        std::vector<DigitalObj3D> objects;                                      // All connected components are going to be stored in it
-        std::back_insert_iterator<std::vector<DigitalObj3D>> inserter(objects); // Iterator used to populated "objects".
-
-        unsigned int nbc = digitalObj.writeComponents(inserter);
-
-        std::cout << "Number of components : " << nbc << std::endl;
+    // I want my programm to iterate on all the scenes
+    for (const auto &scene : scenes3D)
+    {
+        std::cout << "\033[1;32m === " + scene + " analysis ===\033[0m" << std::endl;
 
         // 3D viewer
         QApplication application(argc, argv);
@@ -46,10 +63,27 @@ int main(int argc, char **argv)
         typedef Viewer3D<> MyViewer;
         MyViewer viewer;
         viewer.show();
+
+        // read a 3D image
+        Image3D image = VolReader<Image3D>::importVol("../3D_images/" + scene + "-128.vol");
+
+        // make the foreground and background digital sets from the image
+        Z3i::DigitalSet set_foreground(image.domain()); // Create a digital set of proper size
+        Z3i::DigitalSet set_background(image.domain());
+
+        SetFromImage<Z3i::DigitalSet>::append<Image3D>(set_foreground, image, 0, 255); // populate a digital set from the input image
+        SetFromImage<Z3i::DigitalSet>::append<Image3D>(set_background, image, -1, 1);
+
         // viewer << shape;
         viewer << SetMode3D(image.domain().className(), "BoundingBox");
         viewer << set_foreground << image.domain() << MyViewer::updateDisplay;
 
+        // =================== STEP 2 ===================
+        ComponentsCavities<DigObj26_6, DT26_6>("(26, 6) Adjency : ", set_foreground, set_background, dt26_6);
+        ComponentsCavities<DigObj6_26, DT6_26>("(6, 26) Adjency : ", set_foreground, set_background, dt6_26);
+
         application.exec();
+
+        std::cout << std::endl;
     }
 }
